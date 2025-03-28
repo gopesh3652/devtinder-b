ec2641b89a302a765eeca47c83273c2081f0ebf3
gopesh3652 <gopeshkhandelwal10@gmail.com>
2025-03-28 10:22:22 +0530

    Initial commit

--- src/router/payment.js
+++ src/router/payment.js
@@ -58,7 +58,7 @@ paymentRouter.post("/payment/webhook", async (req, res) => {
 		const webhookSecret = process.env.RAZORPAY_WEBHOOK_SECRET;
 		const webhookBody = req.body;
 		// req.get / req.headers  --> for taking headers from request
-		const webhookSignature = req.headers["x-razorpay-signature"];
+		const webhookSignature = req.get("X-Razorpay-Signature");
 
 		const isWebhookValid = validateWebhookSignature(
 			JSON.stringify(webhookBody),
